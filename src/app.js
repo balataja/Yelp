7953ba5b98a56187c9cc2913b158f7ef12cc96dc
James Balata <Balataja@Jamess-MBP.fios-router.home>
2016-08-02 21:07:27 -0400

    adding react-router

--- src/app.js
+++ src/app.js
@@ -1,11 +1,39 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+
 import './app.css';
 import styles from './styles.module.css';
+import 'font-awesome/css/font-awesome.css';
+
+import { browserHistory, Router } from 'react-router'
+
+import makeRoutes from './routes';
+
+const Home = React.createClass({
+  render: function() {
+    return (<div>Hello world</div>)
+  }
+})
+
+const routes = makeRoutes();
 
 const App = React.createClass({
+  get content() {
+        return (<Router 
+            history={browserHistory} 
+            routes={routes} />)
+    },
+
   render: function() {
-    return (<div className={styles.wrapper} >Text text text</div>);
+    return (
+      /*<div className={styles.wrapper} >
+        <h1><i className="fa fa-star"></i>
+        Environment: {__NODE_ENV__}</h1>
+      </div>*/
+      <div style={ {height: '100%' } }>
+        {this.content}
+      </div>
+    );
   }
 });
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const NODE_ENV = process.env.NODE_ENV;
+const dotenv = require('dotenv');
 const isDev = NODE_ENV === 'development';
 
 const webpack = require('webpack');
@@ -27,8 +28,7 @@ config.postcss = [].concat([
   require('cssnano')({})
 ])
 
-const cssModulesName = `${isDev ? '[path][name]__[local]__' : ''}[hash:base64:5]`;
-
+const cssModulesNames = `${isDev ? '[path][name]__[local]__' : ''}[hash:base64:5]`;
 const matchCssLoaders = /(^|!)(css-loader)($|!)/;
 
 const findLoader = (loaders, match) => {
@@ -51,5 +51,40 @@ config.module.loaders.push(newloader);
 cssloader.test =
   new RegExp(`[^module]${cssloader.test.source}`)
 cssloader.loader = newloader.loader
+
+config.module.loaders.push({
+  test: /\.css$/,
+  include: [modules],
+  loader: 'style!css'
+})
+
+const dotEnvVars = dotenv.config();
+const environmentEnv = dotenv.config({
+  path: join(root, 'config', `${NODE_ENV}.config.js`),
+  silent: true,
+});
+const envVariables = Object.assign({}, dotEnvVars, environmentEnv);
+
+const defines =
+  Object.keys(envVariables)
+  .reduce((memo, key) => {
+    const val = JSON.stringify(envVariables[key]);
+    memo[`__${key.toUpperCase()}__`] = val;
+    return memo;
+  }, {
+    __NODE_ENV__: JSON.stringify(NODE_ENV)
+  });
+
+config.plugins = [
+  new webpack.DefinePlugin(defines)
+].concat(config.plugins);
+
+config.resolve.root = [src, modules]
+config.resolve.alias = {
+  'css': join(src, 'styles'),
+  'containers': join(src, 'containers'),
+  'components': join(src, 'components'),
+  'utils': join(src, 'utils')
+}
 
 module.exports = config;
